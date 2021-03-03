0c4215b17a59ce063fd4f38e96073fe6b39a2259
JeanpierreSaldivar <saldivarleon5b@gmail.com>
2021-03-03 12:08:40 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    
+}
+
+rootProject.name = "PruebaTectina"
+
+
+include(":androidApp")
+include(":shared")
+
---
+++ shared/src/commonMain/kotlin/com/saldivar/pruebatectina/shared/Greeting.kt
@@ -0,0 +1,8 @@
+package com.saldivar.pruebatectina.shared
+
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
