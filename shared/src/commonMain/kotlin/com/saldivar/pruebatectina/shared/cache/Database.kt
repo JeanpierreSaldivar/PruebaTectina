9c4a14a1dee7e1fb6e658105b8a114bcc480d214
JeanpierreSaldivar <saldivarleon5b@gmail.com>
2021-03-03 20:42:58 -0500

    terminando los querys tareas

--- shared/src/commonMain/kotlin/com/saldivar/pruebatectina/shared/cache/Database.kt
+++ shared/src/commonMain/kotlin/com/saldivar/pruebatectina/shared/cache/Database.kt
@@ -1,16 +1,60 @@
 package com.saldivar.pruebatectina.shared.cache
 
+import Tareas
 import com.saldivar.pruebatectina.shared.DatabaseDriverFactory
 
 internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
     private val database = AppDatabase(databaseDriverFactory.createDriver())
     private val dbQuery = database.appDatabaseQueries
 
-    fun consultarListaTareas(estadoTarea:Boolean){
-        dbQuery.getAllTareas(estadoTarea)
+    fun consultarListaTareas(estadoTarea:Boolean):List<Tareas>{
+        val tareaDevolver = mutableListOf<Tareas>()
+        val tareas =dbQuery.getAllTareas(estadoTarea).executeAsList()
+        for (item in tareas){
+            item.apply {
+                val tarea = Tareas(id,titulo,descripcion, creacion, finalizacion, estado)
+                tareaDevolver.add(tarea)
+            }
+        }
+        return tareaDevolver
+
     }
 
-    fun consultarEstadoTarea(idTarea: Int){
-        dbQuery
+    fun consultarEstadoTarea(idTarea: Int):Tareas{
+        val tarea = dbQuery.consultarTarea(idTarea).executeAsOne()
+        val objectTarea = Tareas()
+        objectTarea.apply {
+            id = tarea.id
+            titulo = tarea.titulo
+            descripcion = tarea.descripcion
+            creacion = tarea.creacion
+            finalizacion = tarea.finalizacion
+            estado = tarea.estado
+        }
+
+        return objectTarea
+    }
+
+    fun updateEstado(valorActualizar:Boolean,tarea:Tareas){
+        dbQuery.updateEstado(valorActualizar,tarea.id!!)
+    }
+
+     fun insertarNuevaTarea(nuevaTarea:Tareas){
+         dbQuery.insertTarea(nuevaTarea.id,nuevaTarea.titulo!!,nuevaTarea.descripcion!!,
+         nuevaTarea.creacion!!,nuevaTarea.finalizacion!!,nuevaTarea.estado!!)
+     }
+
+    fun consultarUltimaTareaInsertada():Tareas{
+        val tarea = dbQuery.consultarUltimaTarea().executeAsOne()
+        val objectTarea = Tareas()
+        objectTarea.apply {
+            id = tarea.id
+            titulo = tarea.titulo
+            descripcion = tarea.descripcion
+            creacion = tarea.creacion
+            finalizacion = tarea.finalizacion
+            estado = tarea.estado
+        }
+        return objectTarea
     }
 }
--- shared/src/commonMain/kotlin/com/saldivar/pruebatectina/shared/modulos/home/model/objetos/Tareas.kt
+++ shared/src/commonMain/kotlin/com/saldivar/pruebatectina/shared/modulos/home/model/objetos/Tareas.kt
@@ -1,2 +1,2 @@
 data class Tareas(var id:Int ?= null,var titulo:String ?= null, var descripcion:String ?= null,
-                  var creacion:String ?= null,val finalizacion:String ?= null, var estado:Boolean ?= null )
+                  var creacion:String ?= null,var finalizacion:String ?= null, var estado:Boolean ?= null )
